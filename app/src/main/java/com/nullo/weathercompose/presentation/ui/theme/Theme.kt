ab90749da6a81b4df05060c380623a4c04cecd7a
Nu11Object <null.object@vk.com>
2025-10-07 19:03:22 +0400

    style(theme): tweak color palette for better contrast

--- app/src/main/java/com/nullo/weathercompose/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/nullo/weathercompose/presentation/ui/theme/Theme.kt
@@ -12,29 +12,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-
-    surface = Color(0xFF1C1C1C),
+    surface = SurfaceDark,
     onSurface = Color.White,
+    background = BackgroundDark,
+    onBackground = Color.White,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-
-    surface = Color(0xFF1C1C1C),
+    surface = SurfaceDark,
     onSurface = Color.White,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    */
+    background = Color.White,
+    onBackground = Color.Black,
 )
 
 @Composable
